d0c6ad0b28bb004b120f813013767d62385b3864
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2023-10-30 12:07:36 +0100

    Initial commit

---
+++ src/main/java/Floristeria/Main.java
@@ -0,0 +1,9 @@
+package Floristeria;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Floristeria floristeria = new Floristeria();
+            floristeria.run();
+        }
+}
