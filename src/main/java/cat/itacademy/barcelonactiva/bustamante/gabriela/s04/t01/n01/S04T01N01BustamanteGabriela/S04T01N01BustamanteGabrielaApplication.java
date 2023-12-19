e169135ebd72fd11736441fc1b3f2b9b12bdb7ac
GabyB73 <geb.beg.73@gmail.com>
2023-12-19 11:41:14 +0100

    Ejercicio nivel 1

---
+++ src/main/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s04/t01/n01/S04T01N01BustamanteGabriela/S04T01N01BustamanteGabrielaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.bustamante.gabriela.s04.t01.n01.S04T01N01BustamanteGabriela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01BustamanteGabrielaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01BustamanteGabrielaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s04/t01/n01/S04T01N01BustamanteGabriela/S04T01N01BustamanteGabrielaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.bustamante.gabriela.s04.t01.n01.S04T01N01BustamanteGabriela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01BustamanteGabrielaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
