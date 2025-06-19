01a501617a7bd7c6a53d6da862c16a448f42ef58
GabyB73 <geb.beg.73@gmail.com>
2025-06-19 20:50:38 +0200

    Actualizar controlador FrutaController para cumplir con los endpoints del enunciado

--- src/main/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s04/t02/n03/S04T02N03BustamanteGabriela/controllers/FrutaController.java
+++ src/main/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s04/t02/n03/S04T02N03BustamanteGabriela/controllers/FrutaController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/fruta")
 public class FrutaController {
     @Autowired
     private ServicioFruta servicioFruta;
@@ -36,7 +37,7 @@ public class FrutaController {
         List<Fruta> frutas = servicioFruta.getAllFruta();
         return ResponseEntity.ok(frutas);
     }
-    @GetMapping("/{id}")
+    @GetMapping("/getOne/{id}")
     public ResponseEntity<Fruta> searchFrutaById(@PathVariable ObjectId id) {
         Optional<Fruta> frutaOptional = servicioFruta.getFrutaById(id);
 
